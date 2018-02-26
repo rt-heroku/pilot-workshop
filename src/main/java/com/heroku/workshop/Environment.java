e93ddf66d64988cdae2fb417ae82c0776f7c9649
rt-heroku <rt@heroku.com>
2018-02-26 08:51:51 -0500

    Update Environment.java

--- src/main/java/com/heroku/workshop/Environment.java
+++ src/main/java/com/heroku/workshop/Environment.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 public class Environment {
  String env;
  
- @Bean
+ //@Bean
  public String getEnvironment(){
   System.out.println(System.getenv("ENVIRONMENT"));
   return System.getenv("ENVIRONMENT");
